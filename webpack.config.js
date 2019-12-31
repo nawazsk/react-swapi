783ae2832143a538a46a4022daf7f6dfa082ff62
Nawaz Shaik <nawazshaik@WKMIN1596903.global.publicisgroupe.net>
2019-12-31 16:16:00 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+          template:  path.resolve('./index.html'),
+        })
+    ]
+};
